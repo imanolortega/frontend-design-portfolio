a3ab4ec2ced53886a0e79ebe5009c3cc59c583c9
imanol <imanolrtega@gmail.com>
2025-05-21 12:32:56 -0300

    Move Headers config to next.config file

--- next.config.mjs
+++ next.config.mjs
@@ -13,6 +13,20 @@ const nextConfig = {
     compiler: 'modern',
     silenceDeprecations: ['legacy-js-api'],
   },
+  async headers() {
+    return [
+      {
+        source: '/:path*\\.(js|css|woff2|svg|jpg|jpeg|png|webp|avif)',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=31536000, immutable',
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default withMDX(nextConfig);
+
