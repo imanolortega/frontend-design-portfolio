c1277d5a3e318e0edae10aa1b615ab77f2a6753e
imanol <imanolrtega@gmail.com>
2025-05-21 10:52:46 -0300

    Some changes in projects page

--- src/app/work/page.tsx
+++ src/app/work/page.tsx
@@ -1,4 +1,4 @@
-import { Column } from '@/once-ui/components';
+import { Avatar, Button, Column, Flex, Heading, RevealFx, Text } from '@/once-ui/components';
 import { baseURL } from '@/app/resources';
 import { about, person, work } from '@/app/resources/content';
 import { Meta, Schema } from '@/once-ui/modules';
@@ -30,7 +30,34 @@ export default function Work() {
           image: `${baseURL}${person.avatar}`,
         }}
       />
+      <Heading align="center" marginBottom="l" variant="display-strong-s">
+        Últimos Proyectos
+      </Heading>
+      <Column align="center" fillWidth gap="m" marginBottom="xl" textVariant="body-default-l">
+        Algunos de los últimos proyectos personales en los que he trabajado.
+      </Column>
       <Projects />
+      <RevealFx marginBottom="l" delay={0.4} horizontal="center" paddingLeft="12">
+        <Button
+          id="about"
+          data-border="rounded"
+          href="https://github.com/imanolortega"
+          variant="secondary"
+          size="m"
+          arrowIcon
+        >
+          <Flex gap="8" vertical="center">
+            {about.avatar.display && (
+              <Avatar
+                style={{ marginLeft: '-0.75rem', marginRight: '0.25rem' }}
+                src={person.avatar}
+                size="m"
+              />
+            )}
+            Ver más proyectos
+          </Flex>
+        </Button>
+      </RevealFx>
     </Column>
   );
 }
