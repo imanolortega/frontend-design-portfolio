002a22c1d62fdebf27c953e361a940ab214e8f21
imanol <imanolrtega@gmail.com>
2025-06-04 10:17:21 -0300

    Clean code and format

--- src/components/custom-analytics/CustomAnalytics.tsx
+++ src/components/custom-analytics/CustomAnalytics.tsx
@@ -1,24 +1,24 @@
-'use client'
+'use client';
 
-import dynamic from "next/dynamic";
+import dynamic from 'next/dynamic';
 
 const Analytics = dynamic(
-  () => import("@vercel/analytics/react").then((mod) => mod.Analytics),
+  () => import('@vercel/analytics/react').then((mod) => mod.Analytics),
   {
     ssr: false,
-  }
+  },
 );
 const GoogleAnalytics = dynamic(
-  () => import("@next/third-parties/google").then((mod) => mod.GoogleAnalytics),
+  () => import('@next/third-parties/google').then((mod) => mod.GoogleAnalytics),
   {
     ssr: false,
-  }
+  },
 );
 const SpeedInsights = dynamic(
-  () => import("@vercel/speed-insights/next").then((mod) => mod.SpeedInsights),
+  () => import('@vercel/speed-insights/next').then((mod) => mod.SpeedInsights),
   {
     ssr: false,
-  }
+  },
 );
 export default function CustomAnalytics() {
   return (
